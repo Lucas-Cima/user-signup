e5d6a0f653c31aef26d0d53b65c0e167d3e1e4e8
Lucas Cima <lucas.cima92@gmail.com>
2018-03-31 16:16:22 -0500

    rendered first template

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
-from flask import Flask, request
+from flask import Flask, request, redirect, render_template
 
 app = Flask(__name__)
-
 app.config['DEBUG'] = True
 
 
 @app.route("/")
 def index():
-    return 
+    return render_template('index.html')
 
 
 
-@app.route("/", methods=['POST'])
+#@app.route("/welcome", methods=['POST'])
 def welcome():
     return
 
 
+app.run ()
