8ed88c2196537b785a4618c0fe0a4ced5abf3657
Lucas Cima <lucas.cima92@gmail.com>
2018-04-05 19:25:14 -0500

    getting there...

--- main.py
+++ main.py
@@ -4,33 +4,42 @@ app = Flask(__name__)
 app.config['DEBUG'] = True
 
 
-@app.route("/")
+@app.route("/signup", methods=['GET'])
 def display_user_signup():
     return render_template('index.html')
 
 
 
 
-@app.route("/", methods=['POST', 'GET'])
+@app.route("/signup", methods=['POST'])
 def validate_signup():       
     username = request.form['username']
     username_error = ''
+    password = request.form['password']
+    password_error = ''
+    verify = request.form['verify']
+    verify_error = ''
+    email = request.form['email']
+    email_error = ''
     
-    if len('username') > 20 or len('username') < 3:
+    if len(username) > 20 or len(username) < 3:
         username_error = 'Not a Valid Username'
-        username = ''
+
+        return render_template('index.html', username_error=username_error, password_error=password_error, 
+        verify_error=verify_error, email_error=email_error)
+
+
     else:
-        return render_template('index.html', username=username, username_error=username_error)
+        return redirect('/welcome')
 
 
 
 
+@app.route("/welcome", methods=['POST', 'GET'])
+def welcome():
+    username = request.args.get('username')
 
-#@app.route("/welcome", methods=['POST', 'GET'])
-#def welcome():
-    #username = request.args.get('username')
-
-    #return render_template('welcome.html', username=username)
+    return render_template('welcome.html', username=username)
 
 
 
