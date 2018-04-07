442ee5619e574869f8ed0a2083e8409585103aa3
Lucas Cima <lucas.cima92@gmail.com>
2018-04-07 16:46:40 -0500

    almost there

--- main.py
+++ main.py
@@ -4,14 +4,16 @@ app = Flask(__name__)
 app.config['DEBUG'] = True
 
 
-@app.route("/signup", methods=['GET'])
+@app.route("/", methods=['GET'])
 def display_user_signup():
+  
+
     return render_template('index.html')
 
 
 
 
-@app.route("/signup", methods=['POST'])
+@app.route("/", methods=['POST'])
 def validate_signup():       
     username = request.form['username']
     username_error = ''
@@ -25,12 +27,24 @@ def validate_signup():
     if len(username) > 20 or len(username) < 3:
         username_error = 'Not a Valid Username'
 
+    if '' in username:
+        username_error = 'Not a Valid Username'
+    
+    if len(password) > 20 or len(password) < 3:
+        password_error = 'Not a Valid Password'
+    
+    if '' in password:
+        password_error = 'Not a Valid Password'
+    
+    if verify != password:
+        verify_error = 'Passwords do Not Match'
+
+    if not username_error or password_error or verify_error:
+        return redirect("/welcome?username={0}".format(username))
+    
+    else:
         return render_template('index.html', username_error=username_error, password_error=password_error, 
         verify_error=verify_error, email_error=email_error)
-
-
-    else:
-        return redirect("/welcome?username={0}".format(username))
 
 
 
